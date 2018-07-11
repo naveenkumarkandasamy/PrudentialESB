6a9c4a1b2142540eb8843bb6a7ef320fbb3b5f2b
Naveen Kumar K <naveenkumar.kandasamy@accoliteindia.com>
2018-07-11 17:39:52 +0530

    Routes for Payments

--- src/main/java/org/pru/esb/routers/PASToPastelRouter.java
+++ src/main/java/org/pru/esb/routers/PASToPastelRouter.java
@@ -4,33 +4,38 @@ import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.InputStream;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import org.apache.camel.Exchange;
-import org.apache.camel.LoggingLevel;
 import org.apache.camel.Processor;
 import org.apache.camel.builder.RouteBuilder;
-import org.apache.camel.processor.aggregate.AggregationStrategy;
-import org.apache.http.HttpEntity;
 import org.apache.http.entity.ContentType;
 import org.apache.http.entity.mime.HttpMultipartMode;
-import org.apache.http.entity.mime.MultipartEntity;
 import org.apache.http.entity.mime.MultipartEntityBuilder;
-import org.apache.http.entity.mime.content.ContentBody;
 import org.apache.http.entity.mime.content.FileBody;
+import org.apache.log4j.Logger;
 import org.springframework.stereotype.Component;
 
 @Component
 public class PASToPastelRouter extends RouteBuilder {
+	static final Logger logger = Logger.getLogger(PASToPastelRouter.class);
+	static final String PASTEL_TRANSLATOR_WEBSERVICE_ENDPOINT = "http4://localhost:8080/Pastel/api/convertor/pas/pastel";
+	static final String FOLDER_WITH_PASTEL_FORMAT = "file://data/PASOutput";
+	static final String EASY_PAY_FTP_ENDPOINT = "sftp://easypay@41.60.129.96:1122?password=345yp@yZ4mb!@";
+	private SimpleDateFormat simpleDateFormat = new SimpleDateFormat("YYYY_MM_dd_HH_mm_ss");
+	private String dateAsString = simpleDateFormat.format(new Date());
+
 	@Override
 	public void configure() throws Exception {
 
-		//Route 1
-		
-		from("file://data/PASInput").process(new Processor() {
+		// Route 1
+
+		from("file://data/PASInput").log("Recieved File From PAS").process(new Processor() {
 
 			@Override
 			public void process(Exchange exchange) throws Exception {
-
+				logger.debug("File Recieved from PAS");
 				File filetoUpload = exchange.getIn().getBody(File.class);
 				MultipartEntityBuilder multipartEntityBuilder = MultipartEntityBuilder.create();
 				multipartEntityBuilder.setMode(HttpMultipartMode.BROWSER_COMPATIBLE);
@@ -41,15 +46,16 @@ public class PASToPastelRouter extends RouteBuilder {
 				multipartEntityBuilder.build().writeTo(out);
 				InputStream inputStream = new ByteArrayInputStream(out.toByteArray());
 				exchange.getOut().setBody(multipartEntityBuilder.build());
+
 			}
-		}).setHeader(Exchange.HTTP_METHOD, constant("POST"))
-				.toD("http4://localhost:8080/Pastel/api/convertor/pas/pastel")
-				.to("file://data/PASOutput?fileName=response.csv")
-				.end();
-		
-		//Route 2		
-		
-		from("file://data/PASOutput").to("sftp://easypay@41.60.129.96:1122?password=345yp@yZ4mb!@").end();
+
+		}).log("Sending File to Pastel Integator").setHeader(Exchange.HTTP_METHOD, constant("POST"))
+				.toD(PASTEL_TRANSLATOR_WEBSERVICE_ENDPOINT).to(FOLDER_WITH_PASTEL_FORMAT + "?fileName=response_" + dateAsString + ".csv")
+				.log("Recieved File from Pastel Integator"+dateAsString).end();
+
+		// Route 2
+
+		from(FOLDER_WITH_PASTEL_FORMAT).to(EASY_PAY_FTP_ENDPOINT).log("Sent to EasyPayIntegator").end();
 
 		// Use the below for testing. The below one uses file available in the Pastel
 		// Integerator itself. Its converted and forwarded to easy pay.
--- src/main/java/org/pru/esb/routers/PaymentRouter.java
+++ src/main/java/org/pru/esb/routers/PaymentRouter.java
@@ -18,8 +18,10 @@ import org.apache.log4j.Logger;
 
 public class PaymentRouter extends RouteBuilder {
 	static final Logger logger = Logger.getLogger(PASToPastelRouter.class);
-	static final String PAYMENT_IN_ENDPOINT = "http4://localhost:9060/payment/outbound";
-	static final String OUTPUT_FOLDER = "file://data/PaymentOutput";
+	static final String PAYMENT_INBOUND = "http4://10.4.15.7:9060/payment/inbound";
+	static final String PAYMENT_OUTBOUND = "http4://10.4.15.7:9060/payment/outbound";
+	static final String PAYMENT_OUTPUT_FOLDER = "file://data/PaymentOutput";
+	static final String BANK_OUTPUT_FOLDER = "file://data/BankOutput";
 	static final String EASY_PAY_FTP_ENDPOINT = "sftp://easypay@41.60.129.96:1122?password=345yp@yZ4mb!@";
 	private SimpleDateFormat simpleDateFormat = new SimpleDateFormat("YYYY_MM_dd_HH_mm_ss");
 	private String dateAsString = simpleDateFormat.format(new Date());
@@ -29,7 +31,7 @@ public class PaymentRouter extends RouteBuilder {
 
 		// Route 1
 
-		from("file://data/PaymentInput").log("Recieved File From IL").process(new Processor() {
+		from("file://data/PaymentInput").log("Recieved file From IL").process(new Processor() {
 
 			@Override
 			public void process(Exchange exchange) throws Exception {
@@ -47,8 +49,30 @@ public class PaymentRouter extends RouteBuilder {
 
 			}
 
-		}).log("Sending File to Payment Integator").setHeader(Exchange.HTTP_METHOD, constant("POST"))
-				.toD(PAYMENT_IN_ENDPOINT).log("Sent FILE to Payment Integator").end();
+		}).log("Sending file to Payment Integator").setHeader(Exchange.HTTP_METHOD, constant("POST"))
+				.toD(PAYMENT_OUTBOUND).to(PAYMENT_OUTPUT_FOLDER + "?fileName=response_" + dateAsString + ".txt")
+				.log("Received file From Payment Integator. To be sent to Bank.").end();
+
+		from("file://data/BankInput").log("Recieved file From Bank").process(new Processor() {
+
+			@Override
+			public void process(Exchange exchange) throws Exception {
+				// logger.debug("File Recieved from IL");
+				File filetoUpload = exchange.getIn().getBody(File.class);
+				MultipartEntityBuilder multipartEntityBuilder = MultipartEntityBuilder.create();
+				multipartEntityBuilder.setMode(HttpMultipartMode.BROWSER_COMPATIBLE);
+				String filename = (String) exchange.getIn().getHeader(Exchange.FILE_NAME);
+				File file = exchange.getIn().getBody(File.class);
+				multipartEntityBuilder.addPart("file", new FileBody(file, ContentType.MULTIPART_FORM_DATA, filename));
+				ByteArrayOutputStream out = new ByteArrayOutputStream();
+				multipartEntityBuilder.build().writeTo(out);
+				InputStream inputStream = new ByteArrayInputStream(out.toByteArray());
+				exchange.getOut().setBody(multipartEntityBuilder.build());
+
+			}
+
+		}).log("Sending file to Payment Integator").setHeader(Exchange.HTTP_METHOD, constant("POST"))
+				.toD(PAYMENT_INBOUND).to(BANK_OUTPUT_FOLDER + "?fileName=response_" + dateAsString + ".xls").log("Recieved file from Payment Integator. To be sent to IL.").end();
 
 		// Use the below for testing. The below one uses file available in the Pastel
 		// Integerator itself. Its converted and forwarded to easy pay.
